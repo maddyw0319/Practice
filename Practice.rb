7bee52d1ed1e63095ebce56dca1a323787c4500d
Caleigh Plunkett <leafschool@Students-MacBook-Pro.local>
2019-05-16 10:09:43 -0400

    first commit

---
+++ Practice.rb
@@ -0,0 +1,23 @@
+# puts "do you want cake or candy?"
+# input = gets.chomp
+# if input =="cake" || input =="candy"
+# puts "you've entered my two favorite sweets"
+# else puts "please enter my favorite sweets"
+# end
+# puts "pick a number between 1 and 10"
+# input = gets.chomp.to_i
+# if input > 7 && input < 9
+#   puts "you caught your bus"
+# else
+#   puts "you missed your bus"
+# end
+goat= rand(4)
+if goat== 0
+puts "the number was 0"
+elsif goat== 1
+puts "the number was 1"
+elsif goat== 2
+puts "the number was 2"
+else goat== 3
+puts "everything else failed the number was 3"
+end
